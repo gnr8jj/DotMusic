02be11618311ae566d00862e5803852b8a2cf302
jae2x <isthathimtho@gmail.com>
2026-04-18 00:37:50 +0800

    try lungs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import '@/global.css';
 import { Link } from 'expo-router';
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className='flex-1 p-5' bg-coloraccent> 
+
       <Text className="text-xl font-bold text-maintext">
-        Welcome to Nativewind!
+        DotMusic
       </Text>
       <Link href="/welcome" className="text-maintext rounded bg-primary px-10 py-5 font-medium">
         What
@@ -21,6 +25,6 @@ export default function App() {
       <Link href="/librarytabs/[albums]" className="text-maintext rounded bg-primary px-10 py-5 font-medium">
         BULLY - Kanye West
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
